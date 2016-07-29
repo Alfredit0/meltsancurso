7afcdd2242f987fa6df966bc9d04fb4633ce589d
Meltsan <meltsan@localhost.localdomain>
2016-07-29 12:41:44 -0500

    Clase 3

--- clase3/practicas/Clase3/src/main/java/com/meltsan/curso/App.java
+++ clase3/practicas/Clase3/src/main/java/com/meltsan/curso/App.java
@@ -13,56 +13,55 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App {
 
-	  private static final String PASSWORD="12345";
+	private static final String PASSWORD="12345";
 
-	    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-	        BufferedReader dataIn = new BufferedReader(new
-	                InputStreamReader( System.in) );
+		BufferedReader dataIn = new BufferedReader(new
+				InputStreamReader( System.in) );
 
-	        String username = "";
-	        String password = "";
-	        long start =System.currentTimeMillis();
-	        do {
-	          System.out.println("Ingresa el nombre de usuario: ");
+		String username = "";
+		String password = "";
+		long start =System.currentTimeMillis();
+		do {
+			System.out.println("Ingresa el nombre de usuario: ");
+			// Read data into name variable
+			try{
+				username = dataIn.readLine();
+				username=notBlank(username);
+			}catch( IOException e ){
+				System.out.println("Error!");
+			}
+		} while (username.equals(""));
 
-	          // Read data into name variable
-	          try{
-	              username = dataIn.readLine();
-	              username=notBlank(username);
-	          }catch( IOException e ){
-	              System.out.println("Error!");
-	          }
-	        } while (username.equals(""));
 
+		for(int i=1;i<=3 &&!PASSWORD.equals(password);i++){
+			System.out.println("Ingresa la contraseña: ");
 
-	        for(int i=1;i<=3 &&!PASSWORD.equals(password);i++){
-	          System.out.println("Ingresa la contraseña: ");
+			// Read data into name variable
+			try{
+				password = dataIn.readLine();
+			}catch( IOException e ){
+				System.out.println("Error!");
+			}
+		}
 
-	          // Read data into name variable
-	          try{
-	              password = dataIn.readLine();
-	          }catch( IOException e ){
-	              System.out.println("Error!");
-	          }
-	        }
+		if(PASSWORD.equals(password)
+				){
+			System.out.println("Bienvenido "+username);
+		}else{
+			System.out.println("Completo los 3 intentos");
+		}
+		long end =System.currentTimeMillis();
+		System.out.println(end-start);	        
+	}
 
-	        if(PASSWORD.equals(password)
-	        ){
-	          System.out.println("Bienvenido "+username);
-	        }else{
-	          System.out.println("Completo los 3 intentos");
-	        }
-	        long end =System.currentTimeMillis();
-	        System.out.println(end-start);	        
-	    }
-	    
-	    private static String notBlank(String s){
-	    	if(s.equals(null)){
-	    		return "";
-	    	}else{
-	    	return s.trim();
-	    	}
-	    }
+	private static String notBlank(String s){
+		if(s.equals(null)){
+			return "";
+		}else{
+			return s.trim();
+		}
+	}
 
 }
